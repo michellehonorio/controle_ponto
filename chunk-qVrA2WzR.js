3b0cc571cdb23e052e1583c1acacdc4733eeadd2
michellehonorio <michellehonorio@gmail.com>
2026-09-15 11:47:59 -0300

    Auto-generated commit

---
+++ chunk-qVrA2WzR.js
@@ -0,0 +1,1 @@
+import{T as Vo}from"./main-742CJRKT.js";function o(n){let t=n.replace(/\D/g,``).slice(0,4);return t.length<=2?t:t.slice(0,2)+`:`+t.slice(2)}function l(n){let t=n.replace(/\D/g,``);if(!t)return``;let e=t.padStart(4,`0`).slice(0,4),i=Math.min(23,Number(e.slice(0,2))),s=Math.min(59,Number(e.slice(2)));return Vo(i)+`:`+Vo(s)}export{o as n,l as t};
